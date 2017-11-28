1f9003909b633a101261b65d5ca0ac1c8e4b2ad0
Elvis Li <elvis.li@refineddata.com>
2017-11-28 15:06:24 -0500

    RT-212#Booke: Enrollment Emails Going To Admin Instead of Enrolled User

--- eventlib.php
+++ eventlib.php
@@ -8,7 +8,7 @@ function reminders_role_assigned_event( $ra ) {
     
     if ( !isset( $CFG->studentrole ) ) set_config( 'studentrole', $DB->get_field( 'role', 'id', array( 'archetype'=>'student' ) ) );
     if ( $CFG->studentrole == $ra->objectid ) {
-        $user    = $DB->get_record( 'user', array( 'id'=>$ra->userid ) );
+        $user    = $DB->get_record( 'user', array( 'id'=>$ra->relateduserid ) );
         $context = $DB->get_record( 'context', array( 'id'=>$ra->contextid ) );
         if ( ! $course  = $DB->get_record( 'course', array( 'id'=>$context->instanceid ) ) ) return true;
         if ( empty( $course->welcomemessage ) ) return true;
